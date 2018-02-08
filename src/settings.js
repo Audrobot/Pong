3aac76711e19f5d44514e57a3e9605b9dfa61a8b
Audrobot <kornickiaa@gmail.com>
2018-02-08 14:36:17 -0800

    add p1 p2 keys

--- src/settings.js
+++ src/settings.js
@@ -1,1 +1,9 @@
 export const SVG_NS = 'http://www.w3.org/2000/svg';
+
+export const KEYS = {
+  a: 'a',  //p1      
+  z: 'z',        
+  up: 'ArrowUp', //p2      
+  down: 'ArrowDown',    
+  spaceBar: ' ', 
+}
