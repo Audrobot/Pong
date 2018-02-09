5a25f19b23e4a8a3ce42b558c2e09158f417a7c6
Audrobot <kornickiaa@gmail.com>
2018-02-09 12:50:24 -0800

    increase board size

--- src/partials/Board.js
+++ src/partials/Board.js
@@ -7,7 +7,7 @@ export default class Board {
   }
   render(svg) {
     let rect = document.createElementNS(SVG_NS, 'rect');
-    rect.setAttributeNS(null, 'fill', '#353535');
+    rect.setAttributeNS(null, 'fill', '#567E3A');
     rect.setAttributeNS(null, 'height', this.height);
     rect.setAttributeNS(null, 'width', this.width);
 
@@ -17,12 +17,11 @@ export default class Board {
     line.setAttributeNS(null, 'y1', 0);
     line.setAttributeNS(null, 'x2', (this.width / 2) );
     line.setAttributeNS(null, 'y2', (this.height) );
-    line.setAttributeNS(null, 'stroke', 'red' );
-    line.setAttributeNS(null, 'stroke-width', '2' );
-    line.setAttributeNS(null, 'stroke-dasharray', '20, 15' );
+    line.setAttributeNS(null, 'stroke', 'white' );
+    line.setAttributeNS(null, 'stroke-width', '6' );
+    // line.setAttributeNS(null, 'stroke-dasharray', '10, 15' );
   
     svg.appendChild(rect);
     svg.appendChild(line);
-  
   }
 }
