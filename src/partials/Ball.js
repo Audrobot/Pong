9d9e872ab688c1031ea8b27eeab2221f54b5b901
Audrobot <kornickiaa@gmail.com>
2018-02-09 10:17:03 -0800

    add wall Collision

--- src/partials/Ball.js
+++ src/partials/Ball.js
@@ -13,13 +13,29 @@ export default class Circle {
 reset() {
   this.x = this.boardWidth / 2;
   this.y = this.boardHeight / 2;
+  this.vy = 0;
+  while(this.vy === 0) {
+    this.vy = Math.floor(Math.random() * 10 - 5);
+  }
+  this.vx = this.direction * (6 - Math.abs(this.vy));
+}
+
+wallCollision() {
+  const hitLeft = this.x - this.radius <= 0;
+  const hitRight = this.x - this.radius >= this.boardWidth;
+  const hitTop = this.y - this.radius <= 0;
+  const hitBottom = this.y - this.radius >= this.boardHeight;
+
 }
 
 render(svg, player1, player2) {
+  this.x += this.vx;
+  this.y += this.vy;
+
   let circle = document.createElementNS(SVG_NS, 'circle');
   circle.setAttributeNS(null, 'r', this.radius);
-  circle.setAttributeNS(null, 'cx', this.boardWidth / 2);
-  circle.setAttributeNS(null, 'cy', this.boardHeight / 2);
+  circle.setAttributeNS(null, 'cx', this.x);
+  circle.setAttributeNS(null, 'cy', this.y);
   circle.setAttributeNS(null, 'fill', 'white');
   
   svg.appendChild(circle);
