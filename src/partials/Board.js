b6a9a0a84f0d3597fdd2f261db40cf642141fcb4
Audrobot <kornickiaa@gmail.com>
2018-02-09 15:48:47 -0800

    modify paddle speed

--- src/partials/Board.js
+++ src/partials/Board.js
@@ -18,7 +18,7 @@ export default class Board {
     line.setAttributeNS(null, 'x2', (this.width / 2) );
     line.setAttributeNS(null, 'y2', (this.height) );
     line.setAttributeNS(null, 'stroke', 'white' );
-    line.setAttributeNS(null, 'stroke-width', '6' );
+    line.setAttributeNS(null, 'stroke-width', '10' );
     // line.setAttributeNS(null, 'stroke-dasharray', '10, 15' );
   
     svg.appendChild(rect);
